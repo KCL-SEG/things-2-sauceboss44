98176d492eeaf4103646285695ee077d1862ad1a
sauceboss44 <alexrudakov5@gmail.com>
2023-10-08 18:58:32 +0100

    Add solution for ex1&2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
-
+from django import forms
+from .models import Thing
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['description', 'name', 'quantity']
+        widgets = {'description': forms.Textarea(),'quantity': forms.NumberInput()}
+    description = forms.Textarea()
